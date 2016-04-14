7509ad90ccb899ee37ef563f76c36ef9798c19b0
michael <michael.e.turvey@gmail.com>
2016-04-14 13:49:26 -0700

    this weekend's project

---
+++ what_about_chaps.py
@@ -0,0 +1,8 @@
+what_about_chaps
+howaboutchaps
+
+Have you considered a pair of stylish and functional chaps?
+I have been riding for 18 years and I recommend a pair of chaps to all my friends who want leg protection. And they're stylish!
+
+http://pythonforengineers.com/build-a-reddit-bot-part-1/
+https://praw.readthedocs.org/en/stable/pages/writing_a_bot.html
